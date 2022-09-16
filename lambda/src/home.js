540f8f5ec096e293d9fda38fbcb1a9a5c19ae505
Petr Soukup <soukup@simplia.cz>
2022-09-16 23:43:29 +0200

    page title

--- lambda/src/home.js
+++ lambda/src/home.js
@@ -30,9 +30,9 @@ exports.render = async (user) => {
 <a href="#" class="btn btn-danger btn-lg px-4 terminate-instance"><i class="fa fa-stop"></i> Terminate instance</a>
     `
         }
-        html += `<a href="#" class="btn btn-primary btn-lg px-4 start-instance">Start instance</a>
+        html += `<a href="#" class="btn btn-succes btn-lg px-4 start-instance">Start instance</a>
     `
     }
 
-    return render(html);
+    return render(html, user);
 }
