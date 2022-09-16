81bab23f353c83e96bec32ea2f278e3879654f1e
Petr Soukup <soukup@simplia.cz>
2022-09-17 01:13:37 +0200

    install dependencies

--- lambda/src/user_data.sh
+++ lambda/src/user_data.sh
@@ -32,3 +32,9 @@ echo "%key%" > /home/ec2-user/.ssh/authorized_keys
 chmod 600 /home/ec2-user/.ssh/authorized_keys
 chown -R ec2-user:ec2-user /home/ec2-user/.ssh
 
+yum install -y git
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
