79aa7cb8fca6bd24d6be5994463795f82b69e181
Petr Soukup <soukup@simplia.cz>
2022-09-17 00:13:45 +0200

    find running instance

--- lambda/src/api.js
+++ lambda/src/api.js
@@ -14,6 +14,9 @@ async function findInstance(region, user) {
     }).promise()
 
     console.log(JSON.stringify(list))
+    if (list.Reservations.length === 0) {
+        return null;
+    }
     if (list.Reservations[0].Instances.length === 0) {
         return null
     }
