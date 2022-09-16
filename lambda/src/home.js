58a46a83b98160a51db5490c125dad6c9e939c16
Petr Soukup <soukup@simplia.cz>
2022-09-16 23:01:29 +0200

    loading animation

--- lambda/src/home.js
+++ lambda/src/home.js
@@ -3,7 +3,7 @@ const render = require('./render').render
 exports.render = async (user) => {
 
     let html = `
-    <div class="progress loading">
+    <div class="progress loading" style="display: none">
   <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
 </div>
     <a href="#" class="btn btn-primary btn-lg px-4 start-instance">Start instance</a>
