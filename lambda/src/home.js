4460be00d7085c6f928ff14ef257818f2d48af6b
Petr Soukup <soukup@simplia.cz>
2022-09-16 23:30:43 +0200

    show IP address

--- lambda/src/home.js
+++ lambda/src/home.js
@@ -9,20 +9,21 @@ exports.render = async (user) => {
   <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
 </div>`
 
-    const instance = await api.findInstance('eu-central-1',user)
-    if(!instance || [].indexOf(instance.State.Name)>-1) {
-        if(instance) {
+    const instance = await api.findInstance('eu-central-1', user)
+    if (!instance || [].indexOf(instance.State.Name) > -1) {
+        if (instance) {
             html += `
         <div> Status: ${instance.State.Name}</div>
             <br>
             `
         }
         html += `
+<h4>IP: ${instance.PublicIpAddress}</h4>
 <a href="#" class="btn btn-success btn-lg px-4 hibernate-instance"><i class="fa fa-stop"></i> Stop instance</a>
 &nbsp;
 <a href="#" class="btn btn-success btn-lg px-4 terminate-instance"><i class="fa fa-stop"></i> Terminate instance</a>
     `
-    }else {
+    } else {
         html += `<a href="#" class="btn btn-primary btn-lg px-4 start-instance">Start instance</a>
     `
     }
