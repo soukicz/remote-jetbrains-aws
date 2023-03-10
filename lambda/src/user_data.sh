7f4e2019d6b017f656e6a55757e88c863b22107f
Petr Soukup <soukup@simplia.cz>
2023-03-10 11:59:55 +0100

    reset active connection check on instance start

--- lambda/src/user_data.sh
+++ lambda/src/user_data.sh
@@ -71,6 +71,8 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
+touch /tmp/.active-ssh
+
 cat << EOF > /usr/bin/inactive-poweroff
 #!/bin/bash
 
