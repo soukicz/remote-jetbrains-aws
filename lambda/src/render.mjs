1e20b459b62f74377c3db4b7e11cddf6cf34a78c
Petr Soukup <soukup@simplia.cz>
2023-03-11 18:34:29 +0000

    upgrade to ES6

--- lambda/src/render.mjs
+++ lambda/src/render.mjs
@@ -1,4 +1,4 @@
-const fs = require('fs')
+import { readFileSync } from 'fs'
 
 export default (html, user) => {
     return `<!doctype html>
@@ -47,7 +47,7 @@ export default (html, user) => {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script type="text/javascript">
-    ${fs.readFileSync(__dirname + '/frontend.js', 'utf8')}
+    ${readFileSync(__dirname + '/frontend.js', 'utf8')}
     </script>
       
   </body>
