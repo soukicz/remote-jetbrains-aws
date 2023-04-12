b3595583639136baf071f602d9dbe6a9188241fb
Petr Soukup <soukup@simplia.cz>
2023-04-12 22:14:13 +0200

    install docker compose plugin

--- lambda/src/user_data.sh
+++ lambda/src/user_data.sh
@@ -94,6 +94,10 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
+sudo mkdir -p /usr/local/lib/docker/cli-plugins/
+sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
 systemctl enable docker
 systemctl start docker
 
